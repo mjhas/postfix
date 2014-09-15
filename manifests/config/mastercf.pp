729e260cb7fde8b64f7dfd8a02a7232e8e590c21
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-09-15 15:04:23 +0200

    add test and seperate option handling

--- manifests/config/mastercf.pp
+++ manifests/config/mastercf.pp
@@ -3,12 +3,25 @@
 # define to build up master.cf
 #
 # === Parameters
+#  ensure       = (Default: present)
+#  type         = (Default: 'unix')
+#  private      = (Default: '-')
+#  unprivileged = (Default: '-')
+#  chroot       = (Default: '-')
+#  wakeup       = (Default: '-')
+#  limit        = (Default: '-')
+#  command      = (Default: 'echo',
+#  options      = a hash of option, value pairs, creating '-o <option>=<value>'
+#                 statements after the command. Any options in this hash will
+#                 be placed after any options written directly with the command.
+#                 (Default is empty)
 #
 # === Variables
 #
 # === Authors
 #
 # mjhas@github
+#
 define postfix::config::mastercf (
   $ensure       = present,
   $type         = 'unix',
@@ -17,12 +30,21 @@ define postfix::config::mastercf (
   $chroot       = '-',
   $wakeup       = '-',
   $limit        = '-',
-  $command      = 'echo',) {
+  $command      = 'echo',
+  $options      = {},) {
   Augeas {
     context => '/files/etc/postfix/master.cf',
     notify  => Service['postfix'],
     require => Exec['postfix'],
   }
+
+  if is_hash($options) {
+    $option_line = join (prefix (join_keys_to_values($options, '='), '-o '), ' ')
+  } else {
+    warning("parameter options should be a hash of options and values")
+    $option_line = $options
+  }
+  $full_command =  "${command} ${option_line}"
 
   case $ensure {
     present : {
