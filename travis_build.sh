2e2124d7a2ab089354f5342cd6f8b60c9d390409
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-09-19 15:00:09 +0200

    add proper bundle command

--- travis_build.sh
+++ travis_build.sh
@@ -7,5 +7,5 @@ echo -n 'TRAVIS_BUILD_DIR: ' && echo $TRAVIS_BUILD_DIR
 
 bundle install && \
 bundle exec rake spec_clean spec_prep spec_standalone SPEC_OPTS='--format documentation' && \
-bundle rake lint && \
+bundle exec rake lint && \
 puppet apply --modulepath=$TRAVIS_BUILD_DIR/spec/fixtures/modules --noop tests/init.pp
