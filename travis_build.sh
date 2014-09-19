669324bc1e1ea389d8073c5283a08b14c47df011
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-09-19 13:47:18 +0200

    pull travis build commands into separate file

---
+++ travis_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -n 'PATH: ' && echo $PATH 
+echo -n 'TRAVIS_BUILD_DIR: ' && echo $TRAVIS_BUILD_DIR  
+
+bundle install && \
+bundle exec rake spec_clean spec_prep spec_standalone SPEC_OPTS='--format documentation' && \
+bundle rake lint && \
+puppet apply --modulepath=$TRAVIS_BUILD_DIR/spec/fixtures/modules --noop tests/init.pp
